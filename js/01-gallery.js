60307131497e00b64fff6cef530ac0b5cf3f5c65
VitaliyGO <shuricks007@gmail.com>
2022-08-24 22:10:07 +0300

    added markup to 01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGallery(galleryItems);
+
+gallery.innerHTML += galleryMarkup;
+
+function createGallery(galleryArr) {
+    return galleryArr.map(({preview, original, description}) => {
+        return `<div class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                        />
+                    </a>
+                </div>`
+    }).join('');
+}
+
+
+
+// <div class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+// </div>
