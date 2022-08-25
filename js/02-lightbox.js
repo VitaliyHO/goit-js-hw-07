c1137e395f4f12e3d9c76c191c19f34434aafa15
VitaliyGO <shuricks007@gmail.com>
2022-08-26 00:03:23 +0300

    create gallery with simple lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+const galleryLink = document.querySelector('.gallery>a');
+
+const galleryMarkup = createGallery(galleryItems);
+
+gallery.innerHTML += galleryMarkup;
+
+function createGallery(galleryArr) {
+    return galleryArr.map(({preview, original, description}) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`
+    }).join('');
+};
+
+gallery.addEventListener('click', toImageClick);
+
+function toImageClick(event) {
+    event.preventDefault();
+    if(event.target.nodeName !== 'IMG'){
+        return;
+    }
+};
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: '250' });
